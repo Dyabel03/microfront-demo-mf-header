8b6f03fa296ff8e4ca657288629146d6408e8cae
eugarcia <eugarcia@bancolombia.com.co>
2021-08-27 16:12:36 -0500

    added events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { singleSpaPropsSubject } from 'src/single-spa/single-spa-props';
+
 
 @Component({
   selector: 'mf-header',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mf-header';
+
+  singleSpaProps?: any;
+  subscription?: Subscription;
+  logged = false;
+  user?:string;
+
+  constructor(private ChangeDetectorRef:ChangeDetectorRef){
+
+  }
+
+  ngOnInit(): void {
+    this.initSubscription();
+  }
+
+  initSubscription(){
+    this.subscription = singleSpaPropsSubject.subscribe(
+      props => {
+        this.singleSpaProps = props;
+        this.lookForEvents();
+      }
+    );
+  }
+
+  lookForEvents(){
+    this.singleSpaProps['EventBus'].on('onUserLogged', this.onUserLogged.bind(this));
+  }
+
+  onUserLogged(data: any) {
+    console.log("on user logged listening header ", data);
+    this.logged = true;
+    this.user = data.email;
+    this.ChangeDetectorRef.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
